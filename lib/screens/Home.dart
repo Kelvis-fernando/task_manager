72ed597f8dbd6d0a1b836fcb2265bfdaa0c14fbb
Kelvis Delapena <kelvisdelapena@gmail.com>
2023-05-11 18:36:22 -0300

    chore: finish config the app and AppBar

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager/constants/color.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -6,7 +7,29 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Todo App')),
+      backgroundColor: tdBGColor,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: tdBGColor,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Icon(
+              Icons.menu,
+              color: tdBlack,
+              size: 30,
+            ),
+            Container(
+              height: 40,
+              width: 40,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.asset('assets/images/avatar.jpg'),
+              ),
+            )
+          ],
+        ),
+      ),
       body: Container(
         child: Text('This is home screen'),
       ),
